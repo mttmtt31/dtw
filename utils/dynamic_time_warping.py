14fe22aa54cad100cde5fffd1b19bb66349b90e1
mttmtt31 <matteomatteotti1@gmail.com>
2023-03-01 12:35:24 +0100

    fix: no more nans

--- utils/dynamic_time_warping.py
+++ utils/dynamic_time_warping.py
@@ -5,31 +5,38 @@ from utils.commons import compute_distance
 from tqdm import tqdm
 import pandas as pd
 from typing import Union
+import logging
 
-def dtw(x:pd.Series, y:pd.Series, distance:str="euclidean", warp:int=1, w:Union[int,float]=inf):
+def dtw(event_data:pd.DataFrame, track_data:pd.DataFrame, distance:str="euclidean", warp:int=1, w:Union[int,float]=inf):
     """Computes Dynamic Time Warping (DTW) of two sequences.
 
     Args:
-        x, y (pd.Series): Two pd.Series containing values for `x` coordinate, `y` coordinate, and `time_seconds`
+        event_data, track_data (pd.DataFrame): Two pd.DataFrame containing values for `x` coordinate, `y` coordinate, and `time_seconds`
         distance (str, optional): distance to use. Defaults to "euclidean".
         warp (int, optional): how many shifts are computed. Defaults to 1.
         w (Union[int,float], optional): window size, defined on every row. When inf, the whole row will be considered. Defaults to inf.
 
     Raises:
-        ValueError: when either pd.Series is empty
+        ValueError: when either pd.DataFrame is empty
         ValueError: when the window size is smaller than 0 or it is a float and not inf.
 
     Returns:
         minimum distance, the cost matrix, the accumulated cost matrix, and the wrap path
     """
-    if not len(x) or not len(y):
+    if len(track_data) < len(event_data):
+        logging.warn('Event sequence is longer than tracking sequence. Maybe you pass them in the wrong order?')
+    if not len(event_data) or not len(track_data):
         raise ValueError("Both sequences should be both non-empty")
     if w < 0:
         raise ValueError("Window size should be positive") 
     elif isinstance(w, float) and w < inf:
         raise ValueError(f"Window size can only be an integer or float. {w} is not a valid value.")   
+    event_data = event_data.reset_index()
+    track_data = track_data.reset_index()
     # retrieve the lengths of the two series
-    r, c = len(x), len(y)
+    r, c = len(event_data), len(track_data)
+    # retrieve possible missing values in the tracking sequence
+    missing_indices = track_data[track_data[["x", "y"]].isna().any(axis=1)].index
     # initialise a matrix, of dimension r+1, c+1.
     # The first row and column of this matrix will be initialised to inf, but the value in the top-left corner of the matrix.
     # For each row, the values inside the window will be initialised to zero.
@@ -40,7 +47,7 @@ def dtw(x:pd.Series, y:pd.Series, distance:str="euclidean", warp:int=1, w:Union[
         for i in range(1, r + 1):
             # compute the window of values which will be set to 0.
             # the window is centred in the index of the row in the tracking dataframe with the same time_seconds as the current event you are analysing
-            centre_point = y[y["time_seconds"] == x.loc[i - 1]['time_seconds']].index[0]
+            centre_point = track_data[track_data["time_seconds"] == event_data.loc[i - 1]['time_seconds']].index[0]
             # set to zero all values inside this window
             D0[i, max(1, centre_point - w + 1):min(c + 1, centre_point + w + 1)] = 0
         # set to zero the value in the top-left corner
@@ -51,23 +58,25 @@ def dtw(x:pd.Series, y:pd.Series, distance:str="euclidean", warp:int=1, w:Union[
         D0 = zeros((r + 1, c + 1))
         D0[0, 1:] = inf
         D0[1:, 0] = inf
+    # set to infinity all the values corresponding to missing tracking coordinates
+    D0[:, missing_indices] = inf
     # get rid of the first row and column
     D1 = D0[1:, 1:]  # view
     # for every row
     for i in tqdm(range(r), desc = 'Scanning events... this may take some time'):
         # retrieve the centre point. This will be used to find which values are inside the window previously initialised to zero.
-        centre_point = y[y["time_seconds"] == x.loc[i]['time_seconds']].index[0]
+        centre_point = track_data[track_data["time_seconds"] == event_data.loc[i]['time_seconds']].index[0]
         # for every value different from inf on the current row
         for j in range(c):
-            if (isinf(w) or (max(0, centre_point - w) <= j <= min(c, centre_point + w))):
+            if (isinf(w) or ((max(0, centre_point - w) <= j <= min(c, centre_point + w)) and j not in missing_indices)):
                 # compute the distance between the i-th event of the first sequence and the j-th event of the second sequence 
-                D1[i, j] = compute_distance(x.loc[i], y.loc[j], distance = distance)
+                D1[i, j] = compute_distance(event_data.loc[i], track_data.loc[j], distance = distance)
     # the cost matrix is now ready, copy it.
     C = D1.copy()
     # for each row
     for i in range(r):
         # retrieve the centre of the window on this row
-        centre_point = y[y["time_seconds"] == x.loc[i]['time_seconds']].index[0]
+        centre_point = track_data[track_data["time_seconds"] == event_data.loc[i]['time_seconds']].index[0]
         # find the values inside the window
         if not isinf(w):
             jrange = range(max(0, centre_point - w), min(c, centre_point + w + 1))
@@ -75,19 +84,17 @@ def dtw(x:pd.Series, y:pd.Series, distance:str="euclidean", warp:int=1, w:Union[
             jrange = range(c)
         # for each value inside the window
         for j in jrange:
-            # retrieve the cost for MATCH, INSERTION, DELETION. 
-            # Parameter warp tells you how much you can go back/foward in time. 
-            min_list = [D0[i, j]]
-            for k in range(1, warp + 1):
-                i_k = min(i + k, r)
-                j_k = min(j + k, c)
-                min_list += [D0[i_k, j], D0[i, j_k]]
-            # add the cost of min(insertion, deletion, match) to the cost
-            D1[i, j] += min(min_list)
-    if len(x) == 1:
-        path = zeros(len(y)), range(len(y))
-    elif len(y) == 1:
-        path = range(len(x)), zeros(len(x))
+            if j not in missing_indices:
+                min_list = [D0[i, j]]
+                for k in range(1, warp + 1):
+                    i_k = min(i + k, r)
+                    j_k = min(j + k, c)
+                    min_list += [D0[i_k, j], D0[i, j_k]]
+                D1[i, j] += min(min_list)
+    if len(event_data) == 1:
+        path = zeros(len(track_data)), range(len(track_data))
+    elif len(track_data) == 1:
+        path = range(len(event_data)), zeros(len(event_data))
     else:
         path = _traceback(D0)
     return D1[-1, -1], C, D1, path
@@ -140,12 +147,15 @@ def _traceback(D):
     p, q = [i], [j]
     while (i > 0) or (j > 0):
         tb = argmin((D[i, j], D[i, j + 1], D[i + 1, j]))
+        # matching
         if tb == 0:
             i -= 1
             j -= 1
+        # deletion
         elif tb == 1:
             i -= 1
-        else:  # (tb == 2):
+        # insertion
+        else: 
             j -= 1
         p.insert(0, i)
         q.insert(0, j)
